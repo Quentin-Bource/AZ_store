0bc4f677ddc94f0c3aeed6c6aadc3a4bce1233e7
Quentin <quentin01.bource@gmail.com>
2023-01-17 11:34:14 +0100

    tailwindok

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="output.css" rel="stylesheet" type="text/css">
     <title>A-Z Store</title>
 </head>
-<body class="bg-orange-200">
+<body class="bg-gray-900">
     <header>
             <h2>AZ[Store]</h2>
             <nav class="navbar">
