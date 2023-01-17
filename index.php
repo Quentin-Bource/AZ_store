a7bb3546a1ca00d4c384c21c7b80e7fb0d3438b3
Quentin <quentin01.bource@gmail.com>
2023-01-17 12:09:16 +0100

    début grid

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
     <link href="output.css" rel="stylesheet" type="text/css">
     <title>A-Z Store</title>
 </head>
-<body class="bg-gray-900">
-    <header>
-            <h2>AZ[Store]</h2>
-            <nav class="navbar">
+<body class="bg-gray-900 text-white">
+    <header class="grid grid-cols-3 grid-rows-1 ">
+            <h2 class="grid col-span-1 grid-rows-1">AZ[Store]</h2>
+            <nav class="navbar grid col-span-2 grid-rows-1">
                 <ul>
                     <li class="home">Home</li>
                     <li class="about">About</li>
                     <li class="product">Products</li>
                     <li class="contact">Contact</li>
                 </ul>
-                <button class="panier"><a href="/assets/images/cart2.svg"></a></button>
-                <button class="login">Login</button>
+                <button class="panier grid col-span-3 grid-rows-1"><a href="/assets/images/cart2.svg"></a></button>
+                <button class="login grid col-span-3 grid-rows-1">Login</button>
 
             </nav>
     </header>
