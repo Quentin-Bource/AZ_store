65b6bc332cb6d1ee300c9245165d746399eacb11
Edouard de Romrée <deromree.edouard@gmail.com>
2023-01-18 10:48:54 +0100

    Essaies

--- index.php
+++ index.php
@@ -75,6 +75,7 @@ $shoes =
 foreach ($shoes as $shoe){
     echo '<div class="'.$shoe['product'].'">';    
     echo "<img src=".$shoe['image_url']."><div>";
+    echo '<p>'.$shoe['product'].'</p>';
     echo '<p>'.$shoe['price'].'</p>';
     echo '<form method="post" >';
     echo '<input type="hidden" name="shoe_id" value="'.$shoe["id"].'">';
